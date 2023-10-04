de057eec874141853e58242fb4d2229d421ae110
Hendra Maulidan <hendra0maulidan@gmail.com>
2023-10-04 21:36:25 +0700

    ganti domain lagi

--- resources/views/api/api-masuk.blade.php
+++ resources/views/api/api-masuk.blade.php
@@ -1,5 +1,6 @@
 <?php 
 use Illuminate\Support\Facades\Cache;
+use GuzzleHttp\Exception\RequestException;
 
 $config = App\Models\Config::first();
 $kotas = App\Models\Regency::join('regency_domains','regency_domains.regency_id','=','regencies.id')
@@ -11,16 +12,28 @@ $dataApi = [];
 $i = 0;
 foreach ($kotas as $hehe) : 
     $url = "https://".$hehe->domain."/api/public/get-voice?jenis=suara_masuk";
+
     $voices = Cache::get($url, function () use ($client, $url) {
+        try{
+
+
         $response = $client->request('GET', $url, [
             'headers' => [
                                             'Authorization' => 'Bearer '.'123789',
                                             'Accept' => 'application/json',
                                         ],
         ]);
+
         $voices = json_decode($response->getBody());
+       if($response->getStatusCode() != 200){
+        $voices = "";
+       }
         Cache::put($url, $voices, 60);
         return $voices;
+    }catch(RequestException $e){
+        $voices = "";
+        Cache::put($url, $voices, 60);
+    }
     });
     array_push($dataApi, $voices);
 ?>
@@ -30,9 +43,13 @@ foreach ($kotas as $hehe) :
             <?= $hehe->name  ?>
         </a>      
     </th>
+    @if($voices == "")
+    @else
     @foreach($voices as $vcs)
     <td>{{$vcs->voice}}</td>
     @endforeach
+    @endif
+   
 </tr>
 <?php endforeach; ?>
                                  
